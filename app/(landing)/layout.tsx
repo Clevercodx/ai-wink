33d3267128499af9501edb2156b8e58dc76a3104
Clevercodx <morrismellon2@gmail.com>
2023-07-29 12:24:33 -0700

    free

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ToasterProvider } from '@/components/toaster-provider'
+import { ModalProvider } from '@/components/modal-provider'
+
+
+
 const LandingLayout = ({
   children
 }: {
